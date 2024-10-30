5f9eb8abd53a7e77ef46543dfb981720e6406be9
Arjun Kochhar <akochhar@omnissa.com>
2024-10-30 10:46:04 -0400

    Automate adding user loki

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ $# -ne 1 ]; then
-   echo "Run setup.sh with: 
+   echo "Run setup.sh with:
 	tunall: if all tunnel components need to run
 	tunall-syslog: if everything except syslog needs to be deployed
 	tunall-sys-gra: if everthing except syslog and grafana needs to be deployed
@@ -10,15 +10,22 @@ if [ $# -ne 1 ]; then
    exit 1
 fi
 
+if id -u "loki" >/dev/null 2>&1; then
+    echo 'loki user exists'
+else
+    echo 'loki user missing'
+    useradd loki
+fi
+
 case "$1" in
   "tunall")
-       docker-compose up --build --force-recreate -d; shift;;
-  "tunall-sys") 
-       docker-compose up --build --force-recreate -d influxdb telegraf grafana loki promtail; shift;;
-  "tunall-sys-gra")  
-       docker-compose up --build --force-recreate -d influxdb telegraf loki promtail; shift;;
+       docker compose up --build --force-recreate -d; shift;;
+  "tunall-sys")
+       docker compose up --build --force-recreate -d influxdb telegraf grafana loki promtail; shift;;
+  "tunall-sys-gra")
+       docker compose up --build --force-recreate -d influxdb telegraf loki promtail; shift;;
   "ws1all")
-       docker-compose -f docker-compose-glp.yml up --build --force-recreate -d; shift;;  
-  "clean")  
-       docker-compose down --remove-orphans; shift;; 
+       docker compose -f docker-compose-glp.yml up --build --force-recreate -d; shift;;
+  "clean")
+       docker compose down --remove-orphans; shift;;
 esac
